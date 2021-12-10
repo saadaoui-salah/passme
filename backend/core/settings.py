fd7cb438832871d7571bbc9aa451469c9fb71a1e
salahsaadaoui8@gmail.com <salahsaadaoui8@gmail.com>
2021-12-09 18:52:16 -0800

    add corse headers & auth middleware

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = ["*"]
 
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,11 +45,13 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'password.middleware.IsAuthenticatedMiddleware'
 ]
 
 #CORS_ALLOW_ALL_ORIGINS = True
