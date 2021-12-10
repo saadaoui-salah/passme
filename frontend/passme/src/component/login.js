37016e34e6019c495153f5139c77c83d48c64dc8
salahsaadaoui8@gmail.com <salahsaadaoui8@gmail.com>
2021-12-09 18:55:21 -0800

    update login pagestyle

--- frontend/passme/src/component/login.js
+++ frontend/passme/src/component/login.js
@@ -1,20 +1,20 @@
 import axios from 'axios'
 import { useState } from 'react'
 
+
 export const Login = () => {
     const [password, setPassword] = useState()
     const onClick = async () => {
         const res = await axios.post("http://127.0.0.1:8000/api/auth", {'pwd':password})
-        console.log(res)
+        localStorage.setItem('token', res.data.token)
     }
     const onChange = (e) => {
         setPassword(e.target.value)
-        console.log(password)
     }
     return (
-        <div style={{display:'block'}}>
-            <h1>Welcome To PassMe</h1>
-            <div style={{width:'100%'}}>
+        <div>
+            <h1 style={{color:'#0af'}}>Welcome To PassMe</h1>
+            <div>
                 <input 
                     style={{width:'70%', height:'30px', fontSize:'1.2rem'}}
                     onChange={onChange}
@@ -23,6 +23,7 @@ export const Login = () => {
                     />
                 <button 
                     onClick={onClick}
+                    className="bg-green bg-focus:outline-none focus:ring-4 focus:ring-green-500 focus:ring-opacity-50"
                     style={{width:'25%', height:'36px', fontSize:'1.2rem'}}
                 >Login</button>
             </div>
