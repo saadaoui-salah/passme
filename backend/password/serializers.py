037f2c0a7fb7f1da13c5406beee0bda1d5d25d53
salahsaadaoui8@gmail.com <salahsaadaoui8@gmail.com>
2021-12-09 18:53:52 -0800

    add serializers

--- backend/password/serializers.py
+++ backend/password/serializers.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
-from .models import Passwords
+from .models import Password
 
 class PasswordSerializer(serializers.Serializer):
     class Meta:
-        model = Passwords
+        model = Password
         fields = ['id','app','category','icon']
 
 class PasswordDetailsSerializer(serializers.Serializer):
     class Meta:
-        model = Passwords
+        model = Password
         fields = ['id','password','email']
 
