b426b15353b3e29fe63317db2bd06a2474db1789
salahsaadaoui8@gmail.com <salahsaadaoui8@gmail.com>
2021-12-09 18:53:36 -0800

    update password model name

--- backend/password/models.py
+++ backend/password/models.py
@@ -8,7 +8,7 @@ class Category(models.Model):
 
 
 
-class Passwords(models.Model):
+class Password(models.Model):
     
     app      = models.CharField(max_length=50)
     category = models.ForeignKey(Category, on_delete=models.CASCADE) 
