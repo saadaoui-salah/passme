ec0e389dd0979c9079df24f5eac1bb97fa1f0d73
salahsaadaoui8@gmail.com <salahsaadaoui8@gmail.com>
2021-12-19 19:33:27 -0800

    get token from local storage

--- frontend/passme/src/config.js
+++ frontend/passme/src/config.js
@@ -1,4 +1,6 @@
+const token = localStorage.getItem('token')
+
 export const conf = {
-    url: 'http://localhost:8000/api/pass/',
-    headers: {"Authorization": "Token 9864c11560e990c7db21ee730724363b84ab89e3"}
+    url: 'http://localhost:8000/api/',
+    headers: {"Authorization": `Token ${token}`}
 }
