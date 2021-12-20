01ad6f59514d9f9a10c07a5d7fcc8de485db9b9e
salahsaadaoui8@gmail.com <salahsaadaoui8@gmail.com>
2021-12-19 19:32:17 -0800

    update url

--- backend/password/urls.py
+++ backend/password/urls.py
@@ -5,5 +5,5 @@ from .views import PasswordApiView, PasswordDetailsApiView, AuthApiView
 urlpatterns = [
     path('auth', AuthApiView.as_view()),
     path('pass/', PasswordApiView.as_view()),
-    path('pass/details/<int:id>', PasswordDetailsApiView.as_view())
+    path('pass-details/<int:id>', PasswordDetailsApiView.as_view())
 ]
